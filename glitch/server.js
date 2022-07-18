235db1481cbaa3ca2e9e00394c3a14beef2b3a55
dq-shuhei_ohno <shuhe_ohno@digiq.co.jp>
2022-07-18 23:57:21 +0900

    Firebaseでの自動実行は諦め(コストがかかりすぎる)

--- glitch/server.js
+++ glitch/server.js
@@ -1,100 +1,143 @@
+/**
+ * This is the main Node.js server script for your project
+ * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
+ */
 
-require('../../libraries/notifyLine');
+const path = require("path");
 
+// Require the fastify framework and instantiate it
+const fastify = require("fastify")({
+  // Set this to true for detailed logging:
+  logger: false,
+});
+
+// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
+
+// Setup our static files
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/", // optional: default '/'
+});
+
+// Formbody lets us parse incoming forms
+fastify.register(require("@fastify/formbody"));
+
+// View is a templating manager for fastify
+fastify.register(require("@fastify/view"), {
+  engine: {
+    handlebars: require("handlebars"),
+  },
+});
+
+// Load and parse SEO data
+const seo = require("./src/seo.json");
+if (seo.url === "glitch-default") {
+  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
+}
+
+/**
+ * Our home page route
+ *
+ * Returns src/pages/index.hbs with data built into it
+ */
+fastify.get("/", function (request, reply) {
+  // params is an object we'll pass to our handlebars template
+  let params = { seo: seo };
+
+  // If someone clicked the option for a random color it'll be passed in the querystring
+  if (request.query.randomize) {
+    // We need to load our color data file, pick one at random, and add it to the params
+    const colors = require("./src/colors.json");
+    const allColors = Object.keys(colors);
+    let currentColor = allColors[(allColors.length * Math.random()) << 0];
+
+    // Add the color properties to the params object
+    params = {
+      color: colors[currentColor],
+      colorError: null,
+      seo: seo,
+    };
+  }
+
+  // The Handlebars code will be able to access the parameter values and build them into the page
+  return reply.view("/src/pages/index.hbs", params);
+});
+
+// Run the server and report out to the logs
+fastify.listen(
+  { port: process.env.PORT, host: "0.0.0.0" },
+  function (err, address) {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+    console.log(`Your app is listening on ${address}`);
+    fastify.log.info(`server listening on ${address}`);
+  }
+);
+
+
+// autoby batch
+const identify = process.env.IDENTIFY;
+const pass = process.env.PASS;
+const linetoken = process.env.LINETOKEN;
+const item1 = process.env.ITEM1;
+const item2 = process.env.ITEM2;
+const item3 = process.env.ITEM3;
+const item4 = process.env.ITEM4;
+const item5 = process.env.ITEM5;
+const maxitems = 5;
+
+const fs = require('fs');
+const request = require('request');
 const puppeteer = require('puppeteer');
 
 let browser = null;
 let page = null;
 
-const maxitems = 5;
+let items = [null, null, null, null, null, ];
+if ('string' == typeof item1) {
+  items[0] = item1;
+}
+if ('string' == typeof item2) {
+  items[1] = item2;
+}
+if ('string' == typeof item3) {
+  items[2] = item3;
+}
+if ('string' == typeof item4) {
+  items[3] = item4;
+}
+if ('string' == typeof item5) {
+  items[4] = item5;
+}
+console.log('items=', items);
 
-module.exports = functions.region('asia-northeast1').https.onRequest(async (request, response) => {
-  const body = await autobuyAmazon(60000);
-  if (null === body) {
-    return response.status(404).end('target not found');
+const notifyLine = async function (argtoken, argmessage, argimagepath) {
+  let formdata = { message: '\n' + argmessage };
+  if ('string' == typeof argimagepath && 0 < argimagepath.length) {
+    formdata['imageFile'] = fs.createReadStream(argimagepath);
   }
-  return response.status(200).end(body);
-});
-
-// module.exports = functions.region('asia-northeast1').runWith({ timeoutSeconds: 539, memory: '1GB' }).pubsub.schedule('every 9 minutes').onRun(async (context) => {
-//   await autobuyAmazon(530000);
-//   return true;
-// });
-
-let linetoken = null;
-
-const autobuyAmazon = async function (argmaxtime) {
-  let body = 'end';
-  try {
-    const asnap = await firestore.collection('amazon').doc(process.env.FUNCTION_TARGET.substr(-4)).get();
-    if (!asnap.exists) {
-      console.log('対象データが無いので即終了');
-      return null;
-    }
-    const targetdata = asnap.data();
-    if (targetdata.linetoken) {
-      linetoken = decryptAESFromUTF8Base64(targetdata.linetoken);
-    }
-    const mail = decryptAESFromUTF8Base64(targetdata.identity);
-    const pass = decryptAESFromUTF8Base64(targetdata.pass);
-    const items = targetdata.items;
-
-    // B07RRK3LGN V2AB
-
-    //const asincode = 'B00SAYCXWG';// 「Amazonの他の出品者」(パターン1)のテスト用
-    //const asincode = 'B07GW283KL';// 通常の購入テスト用
-    //const asincode = 'B07GW283KL';// 「Amazonの他の出品者」(パターン2)の一覧上にAmazonのテスト用
-    //const asincode = 'B00EPZYC0U';// すべての出品を見るテスト用
-
-    const starttime = new Date().getTime();
-    console.log('開始時間=', starttime);
-
-    let targetidx = 0;
-    while (argmaxtime > (new Date().getTime()) - starttime) {
-      console.log('ループ開始', (targetidx+1));
-      let res = null;
-      let item = items[targetidx];
-      if (item) {
-        if (item.buycount < item.usecount) {
-          console.log('購入が完了していないアイテムを検知 行番号' + (targetidx+1), item);
-          res = await checkAmazon(mail, pass, item.id);
-          console.log('res=', res);
-          if (true === res) {
-          //if (null === res) {// テスト用
-            items.buycount++;
-            await asnap.ref.update({ items: items });
-          }
-        }
+  const options = {
+    url: 'https://notify-api.line.me/api/notify',
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': `Bearer ${argtoken}` },
+    formData: formdata,
+    json: true,
+  };
+  const res = await new Promise (function (resolve) {
+    request(options, function (error, response, body) {
+      //console.log('response=', response);
+      if (response.statusCode == 200) {
+        resolve(body);
       }
-      console.log('ループ終了', (targetidx+1));
-      if (null === res) {
-        // 次のアイテムのチェック
-        targetidx++;
-        if ((maxitems-1) < targetidx) {
-          // アイテムは3つまでなので3つのチェックが終わったら30秒待って頭に戻る
-          targetidx = 0;
-          if (500000 > (new Date().getTime()) - starttime) {
-            // 30秒後に再トライ
-            console.log('30秒のインターバルを挟む 次の行番号', (targetidx+1));
-            const timer = await new Promise(function (resolve) {
-              setTimeout(function () {
-                resolve(true);
-              }, 30000);
-            });
-          }
-        }
+      else {
+        resolve(error);
       }
-      // XXX 購入の失敗はしつこく再処理するのでidxはインクリメントしない！
-    }
-    console.log('全処理終了');
-  }
-  catch (error) {
-    body = error.toString();
-  }
-  if (browser) {
-    await browser.close();
-  }
-  return body;
+    });
+  });
+  console.log('notify res=', res);
+  return res;
 };
 
 const checkAmazon = async function (argid, argpass, argitemid) {
@@ -121,14 +164,6 @@ const checkAmazon = async function (argid, argpass, argitemid) {
     console.log('販売元=', seller);
     //if (-1 < seller.indexOf('鳶色')) {// XXX テスト購入用
     if (0 === seller.indexOf('Amazon')) {
-      // const iselement = await page.evaluate(function(selector) {
-      //   return true;
-      // }, '#buy-now-button');
-      // console.log('販売元がAmazonなのでそのままワンクリック購入を試みる');
-      // if (iselement) {
-      //   return await onebuyAmazon();
-      // }
-      // console.log('ワンクリック購入ボタンが無い');
       await notifyLine(linetoken, targetURL + '\nは購入出来る状態です。購入を試みます。');
       console.log('販売元がAmazonなのでそのまま購入を試みる');
 
@@ -172,7 +207,7 @@ const checkAmazon = async function (argid, argpass, argitemid) {
     if (0 === seller.indexOf('Amazon')) {
     //if (-1 < seller.indexOf('port town')) {// XXX テスト購入用
     //if (0 < seller.indexOf('Amazon')) {// XXX このブロック後をテストする時用
-      await notifyLine(linetoken, targetURL + '\nは購入出来る状態です。購入を試みます。');
+      await notifyLine(linetoken, targetURL + '?tag=isurut-22&linkCode=osi&th=1&psc=1&aod=1\nは購入出来る状態です。購入を試みます。');
       console.log('販売者一覧ヘッダーの販売元がAmazonなので購入を試みる');
 
       console.log('販売者一覧ヘッダーからカートに追加');
@@ -200,7 +235,7 @@ const checkAmazon = async function (argid, argpass, argitemid) {
     console.log('販売者一覧=', sellers);
     const sidx = sellers.indexOf('Amazon');
     if (-1 < sidx) {
-      await notifyLine(linetoken, targetURL + '\nは購入出来る状態です。購入を試みます。');
+      await notifyLine(linetoken, targetURL + '?tag=isurut-22&linkCode=osi&th=1&psc=1&aod=1\nは購入出来る状態です。購入を試みます。');
       console.log('販売者一覧にAmazonがあるで購入を試みる 行番号=', (sidx+1));
 
       console.log('販売者一覧からカートに追加');
@@ -229,11 +264,12 @@ const initAmazon = async function () {
     return;
   }
   console.log('ブラウザ初期化');
-  browser = await puppeteer.launch({ headless: true, args: ['--disable-web-security', '--disable-features=IsolateOrigins,site-per-process'], });
-  //browser = await puppeteer.launch({ headless: true, });
+  //browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-web-security', '--disable-features=IsolateOrigins,site-per-process'], });
+  browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox']});
   page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OSX) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53');
-  await page.setViewport({ width: 480, height: 1920, });
+  await page.setViewport({ width: 480, height: 1440, });
+  await page.setDefaultTimeout(60000);
   console.log('ブラウザ初期化 OK');
   return;
 };
@@ -245,6 +281,7 @@ const loginAmazon = async function (argid, argpassd) {
     return;
   }
   console.log('ログイン id=', argid);
+  console.log('ログイン pass=', argpassd);
   await page.goto('https://www.amazon.co.jp/ap/signin?_encoding=UTF8&openid.assoc_handle=jpflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.co.jp%2F%3Fref_%3Dnav_signin');
   await page.waitForSelector('form[name="signIn"]');
   await page.evaluate(function(email, passwd) {
@@ -255,49 +292,10 @@ const loginAmazon = async function (argid, argpassd) {
   await page.waitForSelector('#nav-cart-count');
   authorized = true;
   console.log('ログイン OK');
+  await page.screenshot({ path: 'screenshot.png'});
+  await notifyLine(linetoken, 'ログイン成功', 'screenshot.png');
   return;
 };
-
-// const onebuyAmazon = async function () {
-//   try {
-//     console.log('今すぐ購入をクリック');
-//     await page.evaluate(function(selector) {
-//       return document.querySelector(selector).click();
-//     }, '#buy-now-button');
-//     //await page.waitForTimeout(2000);
-//     //await page.screenshot({ path: 'screenshot.png'});
-//     //await page.waitForSelector('#turbo-checkout-panel-container');
-//     const elementHandle = await page.waitForSelector('iframe#turbo-checkout-bottom-sheet-frame');
-//     const frame = await elementHandle.contentFrame();
-//     console.log('frame=', frame);
-//     console.log('今すぐ購入をクリック OK');
-//
-//     console.log('注文の確定');
-//     await frame.waitForSelector('#turbo-checkout-pyo-button');
-//     await frame.evaluate(function(selector) {
-//       return document.querySelector(selector).click();
-//     }, '#turbo-checkout-pyo-button');
-//     await page.waitForTimeout(40000);
-//     await page.screenshot({ path: 'screenshot.png'});
-//     console.log('注文完了');
-//
-//     console.log('注文が成功したかどうかをチェック');
-//     const success = await page.evaluate(function(selector) {
-//       return document.querySelector(selector).textContent;
-//     }, '#widget-purchaseConfirmationStatus .a-alert-inline-success');
-//     console.log('success=', success);
-//     if (-1 < success.indexOf('注文が確定')) {
-//       console.log('注文成功');
-//       await page.screenshot({ path: 'screenshot.png'});
-//       return true;
-//     }
-//     console.log('注文失敗');
-//   }
-//   catch (error) {
-//     console.log('注文完了出来す', error);
-//   }
-//   return false;
-// };
 
 const buyAmazon = async function (argtargeturl) {
   try {
@@ -321,7 +319,8 @@ const buyAmazon = async function (argtargeturl) {
     }, 'input[name="placeYourOrder1"]');
     await page.waitForSelector('#widget-purchaseConfirmationStatus');
     console.log('注文完了');
-    await notifyLine(linetoken, argtargeturl + '\nは注文完了しました。\n成功したかどうか確認して下さい。\nhttps://www.amazon.co.jp/gp/css/order-history?ref_=nav_orders_first');
+    await page.screenshot({ path: 'screenshot.png'});
+    await notifyLine(linetoken, argtargeturl + '\nは注文完了しました。\n成功したかどうか確認して下さい。\nhttps://www.amazon.co.jp/gp/css/order-history?ref_=nav_orders_first', 'screenshot.png');
 
     console.log('注文が成功したかどうかをチェック');
     const success = await page.evaluate(function(selector) {
@@ -338,6 +337,44 @@ const buyAmazon = async function (argtargeturl) {
   catch (error) {
     console.log('注文完了出来す', error);
   }
-  await notifyLine(linetoken, argtargeturl + '\nは注文失敗しました。リトライします。\n実際に失敗したかどうか確認して下さい。\nhttps://www.amazon.co.jp/gp/css/order-history?ref_=nav_orders_first');
+  await page.screenshot({ path: 'screenshot.png'});
+  await notifyLine(linetoken, argtargeturl + '?tag=isurut-22&linkCode=osi&th=1&psc=1&aod=1\nは注文失敗しました。リトライします。\n実際に失敗したかどうか確認して下さい。\nhttps://www.amazon.co.jp/gp/css/order-history?ref_=nav_orders_first', 'screenshot.png');
   return false;
 };
+
+// autoby Core
+const autobyCore = async function () {
+  console.log('loop batch start', new Date().toLocaleString());
+  await notifyLine(linetoken, '監視をスタート');
+  let targetidx = 0;
+  while(true) {
+    //await notifyLine(linetoken, 'glich実行中2');
+    console.log('ループ開始', (targetidx+1));
+    let res = null;
+    let item = items[targetidx];
+    if (item) {
+      console.log('購入が完了していないアイテムを検知 行番号' + (targetidx+1), item);
+      res = await checkAmazon(identify, pass, item);
+      console.log('res=', res);
+    }
+    console.log('ループ終了', (targetidx+1));
+    if (null === res) {
+      // 次のアイテムのチェック
+      targetidx++;
+      if ((maxitems-1) < targetidx) {
+        // アイテムは5つまでなので5つのチェックが終わったら30秒待って頭に戻る
+        targetidx = 0;
+        // 30秒後に再トライ
+        console.log('30秒のインターバルを挟む 次の行番号', (targetidx+1));
+        const timer = await new Promise(function (resolve) {
+          setTimeout(function () {
+            resolve(true);
+          }, 30000);
+        });
+      }
+    }
+    // XXX 購入の失敗はしつこく再処理するのでidxはインクリメントしない！
+  }
+};
+
+autobyCore();
